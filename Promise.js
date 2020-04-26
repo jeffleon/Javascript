ce2fb62ded90ea85a3840869dca04dc6c23e4407
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-26 02:53:01 -0500

    Promises

---
+++ Promise.js
@@ -0,0 +1,24 @@
+/*
+B1RS5p7asXPvdPTAcWn3P68X89QWUKaM
+*/
+let wordAPI = "https://random-word-api.herokuapp.com/word?number=1";
+let gifAPI ="http://api.giphy.com/v1/gifs/search?&api_key=B1RS5p7asXPvdPTAcWn3P68X89QWUKaM&q=";
+function setup(){
+    noCanvas();
+    fetch(wordAPI)
+    .then(response => {
+        return response.json();
+    })
+    .then(json =>{
+        createElement('h1', json); 
+        return fetch(gifAPI + json);   
+    })
+    .then(response=>{
+        return response.json();
+    })
+    .then(json=> {
+        createImg(json.data[0].images.original.url);
+    })
+    .catch(err => console.log(err));
+    console.log("am in");
+}
